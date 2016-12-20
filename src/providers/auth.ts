0af92b90a573a0a2fd3f43b53cf8550f4da431be
Subrat <longlifepistons@gmail.com>
2016-12-21 00:43:24 +0530

    auth removed 2

--- src/providers/auth.ts
+++ src/providers/auth.ts
@@ -19,106 +19,12 @@
     }
     //LogIn
     login(method:String,email:String,password:String,callback){
-      switch(method)
-      {
-        case 'Email':
-          return firebase.auth().signInWithEmailAndPassword(email,password)/*.then((user)=>{
-            callback(user,null);
-          }).catch((err)=>{
-            callback(null,err);
-          });*/
 
-        case 'Google':
-          let promise:any;
-          GooglePlus.trySilentLogin({
-            scope:'',
-            webClientId:'446843274237-18gjo8peimndukpnns566jqrbh19lk3r.apps.googleusercontent.com',
-            offline:true
-          }).then((creds)=>{
-            let provider = firebase.auth.GoogleAuthProvider.credential(creds.idToken,creds.accessToken);
-            promise =  firebase.auth().signInWithCredential(provider).then((success) =>
-                  {
-                      this.events.publish('done','Google');
-                  }, (error) =>
-                  {
-                      this.events.publish('err','Firebase');
-            });
-          }).catch((err)=>{
-            alert(err);
-            this.googleLogin();
-          });
-          break;
-        case 'Facebook':
-          // let promise:any;
-          // Facebook.getLoginStatus().then((creds)=>{
-          //   alert("Middle");
-          //   if(creds.status == "connected"){
-          //     let facebookCredential = firebase.auth.FacebookAuthProvider.credential(creds.authResponse.accessToken);
-          //     promise =  firebase.auth().signInWithCredential(facebookCredential).then((success) =>
-          //           {
-          //               this.events.publish('done','Google');
-          //           }, (error) =>
-          //           {
-          //               this.events.publish('err','Firebase');
-          //     });
-          //   } else{
-          //     alert("First");
-          //     this.fbLogin();
-          //   }
-          // });
-          this.fbLogin();
-          break;
+    return firebase.auth().signInWithEmailAndPassword(email,password)
 
-      }
-    }
-    //GoogleLogin
-    googleLogin(){
-      let promise:any;
-      GooglePlus.login({
-        scope:'',
-        webClientId:'446843274237-18gjo8peimndukpnns566jqrbh19lk3r.apps.googleusercontent.com',
-        offline:true
-      }).then((creds)=>{
-        let provider = firebase.auth.GoogleAuthProvider.credential(creds.idToken,creds.accessToken);
-        firebase.auth().signInWithCredential(provider).then((success) =>
-              {
-                  this.events.publish('done','Google');
-              }, (error) =>
-              {
-                  this.events.publish('err','Firebase');
-        });
-      }).catch((err)=>{
-        this.events.publish('err','Google');
-      });
+
     }
 
-    disconnect(){
-      GooglePlus.disconnect();
-    }
-    //facebook login
-    fbLogin(){
-      let permissions = new Array<string>();
-      permissions = ["public_profile"];
-      Facebook.login(permissions)
-        .then((creds)=>{
-          let facebookCredential = firebase.auth.FacebookAuthProvider.credential(creds.authResponse.accessToken);
-
-          firebase.auth().signInWithCredential(facebookCredential).then((success) =>
-                {
-                    this.createUserFromFacebook(creds.authResponse.userID);
-                }, (error) =>
-                {
-                    this.events.publish('err','Firebase');
-          });
-        }).catch((err)=>{
-          alert("Second");
-          this.events.publish('err','Facebook');
-        });
-      }
-
-  init(){
-    Facebook.browserInit(this.FB_APP_ID, "v2.8");
-  }
 
     //SignUp
     signUp(email:String,password:String,callback){
@@ -138,44 +44,6 @@
     signOut(){
       firebase.auth().signOut();
     }
-
-    createUserFromFacebook(userId){
-      var self = this;
-
-      // let userId = userID;
-      let params = new Array<string>();
-
-      //Getting name and gender properties
-      Facebook.api("/me?fields=name,gender", params)
-      .then(function(user) {
-        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
-        //now we have the users info, let's save it in the NativeStorage
-        NativeStorage.setItem('user',
-        {
-          userID: userId,
-          name: user.name,
-          gender: user.gender,
-          picture: user.picture,
-          phone:917387920029
-        });
-      }).then(()=>{
-      NativeStorage.getItem('user')
-      .then(function(user){
-        firebase.database().ref('users/'+user.userID).set(user,()=>{
-          self.events.publish('done','Facebook');
-          //this.events.subscribe('do',()=>{
-          //  alert('Done');
-          //})
-        }).catch((err)=>{
-          alert(JSON.stringify(err));
-        });
-      }).catch((err)=>{
-        alert(JSON.stringify(err));
-      });
-    }).catch((err)=>{
-      alert(JSON.stringify(err));
-    });
-    };
 
     //Auth Observer
     authObserver(callback){
@@ -197,6 +65,3 @@
         }
       });
     }
-
-
-  }
