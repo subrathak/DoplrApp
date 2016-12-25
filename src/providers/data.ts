771d454b8ef80052d2c76cce3de35380d0de5f82
warlock <gurukiran.v97@gmail.com>
2016-12-25 12:46:21 +0530

    sockets

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Events } from 'ionic-angular';
+
+declare var io;
 
 declare var firebase;
 
@@ -12,9 +15,34 @@ declare var firebase;
 */
 @Injectable()
 export class DataService {
-
+  socket:any;
+  namespaces:any;
+  sockets:any;
+  events:any;
+  got:number;
+  URL:String="http://localhost:3000/";
   constructor(public http: Http) {
 
   }
+  init(id,phone){
+    this.socket = io(this.URL);
+    this.socket.emit('getTaggedDrops',{
+      id:id,
+      phone:phone
+    });
+    this.socket.on('gotDrops',function(data){
+      data.forEach((namespace,i)=>{
+        this.namespaces.push(namespace);
+      });
+    })
+  }
 
+  addListeners(){
+    this.namespaces.forEach((namespace,i)=>{
+      this.sockets[i] = io(URL+namespace);
+    })
+  }
+  getDrops(amount){
+
+  }
 }
